0197965b0f0cceb69e4da4ffbadf5f3c2d6680a5
uzupis <84919649+uzupis@users.noreply.github.com>
2024-01-20 22:17:20 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,7 @@ export default function Home() {
 						</svg>
 					</span></button>
 					<p>$HUX contract: KT1MT3rZkTE8cN8eGQaNJ2Wg2D2nPPtdxELv</p>
-					<p>Minting contract: KT1Wz92wmPaBmR4eEk9J2iAds2n1NEQTVsFb</p>
+					<p>NFT contract: KT1Wz92wmPaBmR4eEk9J2iAds2n1NEQTVsFb</p>
 					<p> </p>
 					<p>A project by Max Haarich / twitter: UzupisMUC
 				</p>
